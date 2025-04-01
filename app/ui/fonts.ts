67b217f9fe14028093474d06f24a1f84b6c53b8c
johnta0 <22534726+johnta0@users.noreply.github.com>
2025-04-01 15:56:26 +0900

    Add fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,11 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana, M_PLUS_1p } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+// export const mplus1p = M_PLUS_1p({
+//   weight: ['400', '700'],
+//   subsets: ['latin'],
+// });
